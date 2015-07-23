f488397a6c2a84cf7801e227f3dd2e1d864ba4a5
Andrey Klochok <andreyklochok@gmail.com>
2015-07-23 19:37:05 +0300

    Add addtional field in rule

--- src/models/Contact.php
+++ src/models/Contact.php
@@ -24,38 +24,56 @@ class Contact extends \hipanel\base\Model
     public function rules()
     {
         return [
-            [['id', 'obj_id', 'client_id', 'seller_id'],                  'integer'],
-            [['type_id', 'state_id'],                                     'integer'],
-            [['create_time', 'update_time', 'created_date', 'updated_date'], 'date'],
-            [['client', 'seller', 'state', 'type'],                       'safe'],
-            [['email', 'abuse_email'],                                    'email'],
-            [['country', 'country_name', 'province', 'province_name'],    'safe'],
-            [['postal_code'],                                             'safe'],
-            [['city', 'street1', 'street2', 'street3'],                   'safe'],
-            [['voice_phone', 'fax_phone'],                                'safe'],
-            [['icq', 'skype', 'jabber'],                                  'safe'],
-            [['roid', 'epp_id', 'remoteid'],                              'safe'],
-            [['name', 'first_name', 'last_name'],                         'safe'],
-            [['birth_date', 'passport_date'],                             'date'],
-            [['passport_no', 'passport_by', 'organization', 'password'],  'safe'],
-            [['remote'] , 'safe'],
-            [['email_confirmed'],                                         'boolean'],
+            [['id', 'obj_id', 'client_id', 'seller_id'], 'safe'],
+            [['type_id', 'state_id'], 'safe'],
+            [['client_name'], 'safe'],
+            [['create_time', 'update_time', 'created_date', 'updated_date'], 'safe'],
+            [['client', 'seller', 'state', 'type'], 'safe'],
+            [['email', 'abuse_email'], 'email'],
+            [['country', 'country_name', 'province', 'province_name'], 'safe'],
+            [['postal_code'], 'safe'],
+            [['city', 'street1', 'street2', 'street3'], 'safe'],
+            [['voice_phone', 'fax_phone'], 'safe'],
+            [['icq', 'skype', 'jabber'], 'safe'],
+            [['roid', 'epp_id', 'remoteid', 'other_messenger'], 'safe'],
+            [['name', 'first_name', 'last_name', 'voice_phone_extension'], 'safe'],
+            [['birth_date', 'passport_date'], 'date'],
+            [['passport_no', 'passport_by', 'organization', 'password'], 'safe'],
+            [['remote'], 'safe'],
+            [['email_confirmed'], 'boolean'],
+
+            [['first_name', 'last_name', 'email', 'street1', 'city', 'country', 'postal_code', 'postal_code', 'voice_phone'], 'required', 'on' => ['create', 'update']],
+
+            [[
+                // Для регистрации доменов в зоне RU в качестве физического лица
+                'birth_date',
+                'passport_no',
+                'passport_date',
+                'passport_by',
+
+                // Для регистрации доменов в зоне RU в качестве юридического лица
+                'organization_ru',
+                'director_name',
+                'inn',
+                'kpp',
+                'isresident',
+            ], 'safe'],
         ];
     }
 
     public function attributeLabels()
     {
         return $this->mergeAttributeLabels([
-            'create_time'   => Yii::t('app', 'Create time'),
-            'update_time'   => Yii::t('app', 'Update time'),
-            'passport_no'   => Yii::t('app', 'Passport number'),
-            'passport_by'   => Yii::t('app', 'Passport by'),
+            'create_time' => Yii::t('app', 'Create time'),
+            'update_time' => Yii::t('app', 'Update time'),
+            'passport_no' => Yii::t('app', 'Passport number'),
+            'passport_by' => Yii::t('app', 'Passport by'),
             'passport_date' => Yii::t('app', 'Passport date'),
-            'icq'           => 'ICQ',
-            'voice_phone'   => Yii::t('app', 'Phone'),
-            'fax_phone'     => Yii::t('app', 'Fax'),
-            'country_name'  => Yii::t('app', 'Country'),
-            'abuse_email'   => Yii::t('app', 'Abuse email'),
+            'icq' => 'ICQ',
+            'voice_phone' => Yii::t('app', 'Phone'),
+            'fax_phone' => Yii::t('app', 'Fax'),
+            'country_name' => Yii::t('app', 'Country'),
+            'abuse_email' => Yii::t('app', 'Abuse email'),
         ]);
     }
 }
