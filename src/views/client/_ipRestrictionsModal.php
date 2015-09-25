297e27cc075ce416c1f8b6c264b82591c641a83e
Andrey Klochok <andreyklochok@gmail.com>
2015-09-25 19:02:08 +0300

    Add ipRestriction form

--- src/views/client/_ipRestrictionsModal.php
+++ src/views/client/_ipRestrictionsModal.php
@@ -7,7 +7,7 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\web\JsExpression;
 
-$model->scenario = 'ip-restrictions-settings';
+$model->scenario = 'ip-restrictions';
 
 ?>
 
@@ -21,5 +21,29 @@ $model->scenario = 'ip-restrictions-settings';
         'class' => 'clickable',
     ],
 ]); ?>
-    Comming soon...
+
+<?php Pjax::begin(array_merge(Yii::$app->params['pjax'], ['enablePushState' => false])); ?>
+
+    <div class="callout callout-default">
+        Enter comma separated list of IP-addresses or subnets.<br>
+        Example: 88.208.52.222, 213.174.0.0/16<br>
+        Your current IP address is 88.208.49.170<br>
+    </div>
+
+<?php $form = ActiveForm::begin([
+    'action' => Url::to('@client/mailing-settings'),
+    'options' => ['data-pjax' => '1'],
+    'enableClientValidation' => true,
+    'validateOnBlur' => true,
+    'enableAjaxValidation' => true,
+    'validationUrl' => Url::toRoute(['validate-form', 'scenario' => $model->scenario]),
+]); ?>
+
+<?= $form->field($model, 'allowed_ips'); ?>
+<?= $form->field($model, 'sshftp_ips')->hint("All of accounts in the hosting panel will use following permit IP addresses list by default.
+You can reassign permitted IP addresses for each account individually in it's settings."); ?>
+
+<?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-default']) ?>
+<?php $form::end(); ?>
+<?php Pjax::end() ?>
 <?php Modal::end(); ?>
