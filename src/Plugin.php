7da5e112f2503e51b151966c303dbac8f444dac1
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-02-04 14:15:36 +0200

    Added module-scope translations file

--- src/Plugin.php
+++ src/Plugin.php
@@ -26,5 +26,18 @@ class Plugin extends \hiqdev\pluginmanager\Plugin
                 'class' => 'hipanel\modules\client\Module',
             ],
         ],
+        'components' => [
+            'i18n' => [
+                'translations' => [
+                    'hipanel/client' => [
+                        'class' => 'yii\i18n\PhpMessageSource',
+                        'basePath' => '@hipanel/modules/client/messages',
+                        'fileMap' => [
+                            'hipanel/client' => 'client.php',
+                        ],
+                    ],
+                ],
+            ],
+        ],
     ];
 }
