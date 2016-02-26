dd7f75c7af11434ffcf7d03e058539bed785337a
Andrii Vasyliev <sol@hiqdev.com>
2016-02-26 17:24:43 +0000

    minor

--- src/views/client/_search.php
+++ src/views/client/_search.php
@@ -7,37 +7,37 @@ use yii\helpers\Html;
 
 ?>
 
-    <div class="col-md-4">
-        <?= $search->field('client_like') ?>
-        <?= $search->field('name_like')->label(Yii::t('app', 'Name')) ?>
-    </div>
+<div class="col-md-4">
+    <?= $search->field('client_like') ?>
+    <?= $search->field('name_like')->label(Yii::t('app', 'Name')) ?>
+</div>
 
-    <div class="col-md-4">
-        <?= $search->field('seller_like') ?>
-        <?= $search->field('seller_id')->widget(ClientCombo::classname()) ?>
-    </div>
+<div class="col-md-4">
+    <?= $search->field('seller_like') ?>
+    <?= $search->field('seller_id')->widget(ClientCombo::classname()) ?>
+</div>
 
-    <div class="col-md-4">
-        <?= $search->field('state')->widget(StaticCombo::classname(), [
-            'data'          => $states,
-            'hasId'         => true,
+<div class="col-md-4">
+    <?= $search->field('state')->widget(StaticCombo::classname(), [
+        'data'          => $states,
+        'hasId'         => true,
+        'pluginOptions' => [
+            'select2Options' => [
+                'multiple' => true,
+            ],
+        ],
+    ]) ?>
+    <div class="form-group">
+        <?= Html::tag('label', 'Registered range', ['class' => 'control-label']); ?>
+        <?= DatePicker::widget([
+            'model'         => $search->model,
+            'type'          => DatePicker::TYPE_RANGE,
+            'attribute'     => 'created_from',
+            'attribute2'    => 'created_till',
             'pluginOptions' => [
-                'select2Options' => [
-                    'multiple' => true,
-                ],
+                'autoclose' => true,
+                'format'    => 'yyyy-mm-dd',
             ],
         ]) ?>
-        <div class="form-group">
-            <?= Html::tag('label', 'Registered range', ['class' => 'control-label']); ?>
-            <?= DatePicker::widget([
-                'model'         => $search->model,
-                'type'          => DatePicker::TYPE_RANGE,
-                'attribute'     => 'created_from',
-                'attribute2'    => 'created_till',
-                'pluginOptions' => [
-                    'autoclose' => true,
-                    'format'    => 'dd-mm-yyyy',
-                ],
-            ]) ?>
-        </div>
     </div>
+</div>
