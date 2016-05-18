d74d2e200f3c603434d50a219a35847dca631c0b
Andrii Vasyliev <sol@hiqdev.com>
2016-05-18 14:33:27 +0000

    redone to composer-config-plugin

---
+++ src/config/hisite.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * Client module for HiPanel
+ *
+ * @link      https://github.com/hiqdev/hipanel-module-client
+ * @package   hipanel-module-client
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
+return [
+    'aliases' => [
+        '@client'  => '/client/client',
+        '@contact' => '/client/contact',
+    ],
+    'modules' => [
+        'client' => [
+            'class' => 'hipanel\modules\client\Module',
+        ],
+    ],
+    'components' => [
+        'i18n' => [
+            'translations' => [
+                'hipanel/client' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@hipanel/modules/client/messages',
+                    'fileMap' => [
+                        'hipanel/client' => 'client.php',
+                    ],
+                ],
+            ],
+        ],
+        'menuManager' => [
+            'menus' => [
+                'hipanel\modules\client\SidebarMenu',
+            ],
+        ],
+    ],
+];
