6641aebeefe11cdb6e5763519974e21e08ed8a34
Andrii Vasyliev <sol@hiqdev.com>
2017-01-30 13:42:44 +0000

    removed use of ApiConnectionInterface

--- src/repositories/NotifyTriesRepository.php
+++ src/repositories/NotifyTriesRepository.php
@@ -2,7 +2,6 @@
 
 namespace hipanel\modules\client\repositories;
 
-use hipanel\components\ApiConnectionInterface;
 use hipanel\modules\client\models\Contact;
 use hipanel\modules\client\models\NotifyTries;
 use hiqdev\hiart\ErrorResponseException;
@@ -11,16 +10,6 @@ use yii\db\ActiveRecordInterface;
 class NotifyTriesRepository
 {
     /**
-     * @var ApiConnectionInterface
-     */
-    private $api;
-
-    public function __construct(ApiConnectionInterface $api)
-    {
-        $this->api = $api;
-    }
-
-    /**
      * @param Contact|ActiveRecordInterface $contact
      * @param string $type
      * @return NotifyTries|null
@@ -28,7 +17,7 @@ class NotifyTriesRepository
     public function getTriesForContact(Contact $contact, $type)
     {
         try {
-            $data = $this->api->get('contactGetNotifyTries', ['id' => $contact->id, 'type' => $type]);
+            $data = Contact::perform('get-notify-tries', ['id' => $contact->id, 'type' => $type]);
             return new NotifyTries($data);
         } catch (ErrorResponseException $e) {
             return null;
