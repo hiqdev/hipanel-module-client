872b2169bb40c37615b4765ac2aca0e166c9cb5b
pkolomiy <pkolomiy@gmail.com>
2018-05-31 16:28:10 +0300

    Fixed

--- tests/acceptance/admin/ClientSidebarMenuCest.php
+++ tests/acceptance/admin/ClientSidebarMenuCest.php
@@ -4,22 +4,18 @@ namespace hipanel\modules\client\tests\acceptance\admin;
 
 use hipanel\tests\_support\Page\SidebarMenu;
 use hipanel\tests\_support\Step\Acceptance\Admin;
-use yii\helpers\Url;
 
 class ClientSidebarMenuCest
 {
     public function ensureMenuIsOk(Admin $I)
     {
-        $I->login();
         $menu = new SidebarMenu($I);
 
-        $I->amOnPage(Url::to(['/']));
         $menu->ensureContains('Clients', [
-            'Clients' => '/client/client/index',
-            'Contacts' => '/client/contact/index',
+            'Clients' => '@client/index',
+            'Contacts' => '@contact/index',
         ]);
 
-        $I->amOnPage(Url::to(['/']));
         $menu->ensureDoesNotContain('Clients', [
             'Documents',
             'Mailing preparation',
--- tests/acceptance/seller/ClientSidebarMenuCest.php
+++ tests/acceptance/seller/ClientSidebarMenuCest.php
@@ -4,20 +4,15 @@ namespace hipanel\modules\client\tests\acceptance\seller;
 
 use hipanel\tests\_support\Page\SidebarMenu;
 use hipanel\tests\_support\Step\Acceptance\Seller;
-use yii\helpers\Url;
 
 class ClientSidebarMenuCest
 {
     public function ensureMenuIsOk(Seller $I)
     {
-        $I->login();
-        $menu = new SidebarMenu($I);
-
-        $I->amOnPage(Url::to(['/']));
-        $menu->ensureContains('Clients', [
-            'Clients' => '/client/client/index',
-            'Contacts' => '/client/contact/index',
-            'Documents' => '/document/document/index',
+        (new SidebarMenu($I))->ensureContains('Clients', [
+            'Clients' => '@client/index',
+            'Contacts' => '@contact/index',
+            'Documents' => '@document/index',
             'Mailing preparation' => '/mailing/prepare/index',
         ]);
     }
