d34dee64d6cf9f60caeb734df8cfd1221b1bff45
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2018-07-11 17:10:28 +0300

    Fixed import for Domain, accidentall removed in 51ba94a24

--- src/models/Client.php
+++ src/models/Client.php
@@ -12,6 +12,7 @@ namespace hipanel\modules\client\models;
 
 use hipanel\helpers\StringHelper;
 use hipanel\modules\client\models\query\ClientQuery;
+use hipanel\modules\domain\models\Domain;
 use hipanel\modules\finance\models\Purse;
 use hipanel\modules\server\models\Server;
 use hipanel\validators\DomainValidator;
