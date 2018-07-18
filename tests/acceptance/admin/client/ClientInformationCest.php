db7c2e2c9abc71f7a5dc1224784c3953a24d3b96
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2018-07-18 10:13:38 +0300

    Reduce tests fragility

--- tests/acceptance/admin/client/ClientInformationCest.php
+++ tests/acceptance/admin/client/ClientInformationCest.php
@@ -27,12 +27,12 @@ class ClientInformationCest
             [$key => 'type',        'text' => 'Type',        'td' => 'Admin'],
             [$key => 'state',       'text' => 'Status',      'td' => 'Ok'],
             [$key => 'create_time', 'text' => 'Registered',  'td' => 'Feb 27, 2017, 4:56:32 PM'],
-            [$key => 'update_time', 'text' => 'Last update', 'td' => 'Jun 13, 2018, 6:38:41 AM'],
-            [$key => 'tickets',     'text' => 'Tickets',     'td' => ''],
-            [$key => 'servers',     'text' => 'Servers',     'td' => ''],
-            [$key => 'domains',     'text' => 'Domains',     'td' => ''],
+            [$key => 'update_time', 'text' => 'Last update', 'td' => null],
+            [$key => 'tickets',     'text' => 'Tickets',     'td' => null],
+            [$key => 'servers',     'text' => 'Servers',     'td' => null],
+            [$key => 'domains',     'text' => 'Domains',     'td' => null],
             [$key => 'contacts',    'text' => 'Contacts',    'td' => '1 contact'],
-            [$key => 'hosting',     'text' => 'Hosting',     'td' => ''],
+            [$key => 'hosting',     'text' => 'Hosting',     'td' => null],
         ];
         foreach ($tbody as $tr) {
             $I->seeElement(['css' => 'table tbody tr th'], [$key => $tr[$key]]);
