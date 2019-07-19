880f2573e8b9eb484005300e09080a88fe9b9130
Maksym <smy980807@ukr.net>
2019-07-19 12:16:34 +0300

    refactored advancedhosters seller contacts acceptance test (#84)
    * refactored advancedhosters seller contacts acceptance test
    
    * minor
    
    * minor
    
    * minor

--- tests/acceptance/seller/ContactsCest.php
+++ tests/acceptance/seller/ContactsCest.php
@@ -10,84 +10,116 @@
 
 namespace hipanel\modules\client\tests\acceptance\seller;
 
+use Codeception\Example;
 use hipanel\helpers\Url;
 use hipanel\modules\client\tests\_support\Page\contact\Create;
-use hipanel\modules\client\tests\_support\Page\contact\Index;
+use hipanel\modules\client\tests\_support\Page\contact\Update;
+use hipanel\tests\_support\Page\IndexPage;
+use hipanel\tests\_support\Page\Widget\Input\Input;
 use hipanel\tests\_support\Step\Acceptance\Seller;
 
 class ContactsCest
 {
-    /**
-     * @var Index $index
-     */
-    private $index;
-    /**
-     * @var Create $create
-     */
-    private $create;
-    /**
-     * @var string $contactId
-     */
-    private $contactId;
+    /** @var IndexPage */
+    private $indexPage;
+
+    /** @var Create */
+    private $createPage;
+
+    /** @var array */
+    private $createdContacts;
 
     public function _before(Seller $I): void
     {
-        $this->index = new Index($I);
-        $this->create = new Create($I);
+        $this->indexPage = new IndexPage($I);
+        $this->createPage = new Create($I);
     }
 
-    public function ensureIndexPageWorks(Seller $I): void
-    {
-        $I->needPage(Url::to('@contact'));
-        $I->see('Contact', 'h1');
-        $I->seeLink('Create', Url::to('create'));
-        $this->index->ensureICanSeeAdvancedSearchBox($I);
-        $this->index->ensureICanSeeBulkSearchBox();
-    }
-
-    public function ensureICanCreateContact(Seller $I): void
+    /**
+     * @dataProvider testContactData
+     *
+     * @param Seller $I
+     * @param Example $data
+     * @throws \Exception
+     */
+    public function ensureICanCreateContact(Seller $I, Example $data): void
     {
         $I->needPage(Url::to('@contact/create'));
         $I->see('Create contact', 'h1');
-        $testContact = $this->testContactData();
-        $this->create->fillFormData($testContact);
+        $this->createPage->fillFormData($data);
         $I->click('Create contact');
-        $this->contactId = $this->create->seeContactWasCreated();
+        $contactId = $this->createPage->seeContactWasCreated();
+        $this->createdContacts[$contactId] = $data['inputs']['first_name'];
     }
 
+    /**
+     * @param Seller $I
+     * @throws \Codeception\Exception\ModuleException
+     */
+    public function ensureICanUpdateContact(Seller $I): void
+    {
+        $updatePage = new Update($I);
+
+        foreach ($this->createdContacts as $id => $name) {
+            $I->needPage(Url::to('@contact'));
+
+            $this->indexPage->gridView->filterBy(
+                Input::asTableFilter($I, 'Name'), $name
+            );
+            $this->indexPage->gridView->openRowMenuById($id);
+            $this->indexPage->gridView->chooseRowMenuOption('Edit');
+
+            $data['inputs']['last_name'] = 'edited';
+            $updatePage->fillFormData($data);
+            $I->pressButton('Save');
+            $I->closeNotification('Contact was updated');
+        }
+    }
+
+    /**
+     * @param Seller $I
+     * @throws \Exception
+     */
     public function ensureICantCreateIncorrectContact(Seller $I): void
     {
         $I->needPage(Url::to('@contact/create'));
-        $testContact = $this->testContactData();
-        $testContact['inputs']['street1'] = '111111111111111111111111111111111111111111111111111111111111111';
-        $testContact['inputs']['street2'] = '111111111111111111111111111111111111111111111111111111111111111';
-        $testContact['inputs']['street3'] = '111111111111111111111111111111111111111111111111111111111111111';
-        $this->create->fillFormData($testContact);
+        $testContact = $this->testContactData()[0];
+        $testContact['inputs']['street1'] = str_repeat('1', 65);
+        $testContact['inputs']['street2'] = str_repeat('2', 65);
+        $testContact['inputs']['street3'] = str_repeat('3', 65);
+        $this->createPage->fillFormData($testContact);
         $I->click('Create contact');
-        $this->create->seeErrorInAddress();
+        $this->createPage->seeErrorInAddress();
     }
 
+    /**
+     * @param Seller $I
+     * @throws \Codeception\Exception\ModuleException
+     */
     public function ensureICanDeleteContact(Seller $I): void
     {
-        $testContact = $this->testContactData()['inputs'];
-        $I->needPage(Url::to('@contact/index'));
-        $this->index->showCreatedContactOnIndexPage($testContact['first_name']);
-        $this->index->checkBoxClick((string)$this->contactId);
-        $I->click('Delete');
-        $I->acceptPopup();
-        $I->waitForPageUpdate();
-        $I->closeNotification('Contact was deleted');
+        foreach ($this->createdContacts as $id => $name) {
+            $I->needPage(Url::to('@contact'));
+            $this->indexPage->gridView->filterBy(
+                Input::asTableFilter($I, 'Name'), $name
+            );
+            $this->indexPage->gridView->selectRowById($id);
+            $I->pressButton('Delete');
+            $I->acceptPopup();
+            $I->waitForPageUpdate();
+            $I->closeNotification('Contact was deleted');
+        }
     }
 
     /**
      * @return array
      */
-    private function testContactData(): array
+    protected function testContactData(): array
     {
-        return [
+        $basicData = [
             'inputs' => [
-                'first_name'    => 'Test',
-                'last_name'     => 'Reseller',
+                'first_name'    => null,
+                'last_name'     => 'Manager',
                 'email'         => 'hipanel_test_manager@hiqdev.com',
                 'abuse_email'   => 'hipanel_test_manager+abuse@hiqdev.com',
                 'organization'  => 'HiQDev',
@@ -95,7 +127,7 @@ class ContactsCest
                 'city'          => 'Test',
                 'province'      => 'Testing',
                 'postal_code'   => 'TEST',
-                'voice_phone'   => '(445) 456-4561',
+                'voice_phone'   => null,
                 'icq'           => '1002893',
                 'skype'         => 'hipanel_test_manager',
                 'jabber'        => 'hipanel_test_manager@hiqdev.com',
@@ -105,9 +137,37 @@ class ContactsCest
             ],
             'selects' => [
                 'country'       => 'Algeria',
-                'client'        => 'hipanel_test_admin',
+                'client'        => 'hipanel_test_manager',
             ],
-
+            'phoneCountryCode'  => null,
         ];
+
+        $varData = [
+            [
+                'voice_phone'       => '(965) 449-99-99',
+                'phoneCountryCode'  => 'ru',
+            ],
+            [
+                'voice_phone'       => '093 000-1122',
+                'phoneCountryCode'  => 'ua',
+            ],
+            [
+                'voice_phone'       => '+3 8 093 000-1122',
+                'phoneCountryCode'  => 'ua',
+            ],
+        ];
+
+        $data = [];
+
+        foreach ($varData as $varItem) {
+            $tmp = $basicData;
+            $tmp['inputs']['first_name'] = 'Test' . uniqid();
+            $tmp['inputs']['voice_phone'] = $varItem['voice_phone'];
+            $tmp['phoneCountryCode'] = $varItem['phoneCountryCode'];
+
+            $data[] = $tmp;
+        }
+
+        return $data;
     }
 }
