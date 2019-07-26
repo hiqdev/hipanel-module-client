3dc589e867768335f5f1147abef4813f88ed153d
strorch <smy980807@ukr.net>
2019-07-26 14:32:48 +0300

    added 2factor auth check for advancedhosting

--- src/menus/ClientDetailMenu.php
+++ src/menus/ClientDetailMenu.php
@@ -59,7 +59,7 @@ class ClientDetailMenu extends \hipanel\menus\AbstractDetailMenu
                 'label' => $totp_enabled ? Yii::t('hipanel:client', 'Disable two factor authorization') : Yii::t('hipanel:client', 'Enable two factor authorization'),
                 'icon' => 'fa-lock',
                 'url' => 'https://' . Yii::$app->params['hiam.site'] . Url::to(['/mfa/totp/' . ($totp_enabled ? 'disable' : 'enable'), 'back' => Url::to('', true)]),
-                'visible' => $user->is($this->model->id),
+                'visible' => (Yii::$app->params['module.client.twoFactorAuth']) ? $user->is($this->model->id) : false ,
             ],
             [
                 'label' => SettingsModal::widget([
