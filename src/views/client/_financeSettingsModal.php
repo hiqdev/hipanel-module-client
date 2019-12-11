36b70c922a4b69ecf325841683cbf2e0b371f922
Andrii Vasyliev <sol@hiqdev.com>
2019-12-11 15:10:50 +0000

    Allowed exchange to EURO even if user doesn't have the purse

--- src/views/client/_financeSettingsModal.php
+++ src/views/client/_financeSettingsModal.php
@@ -16,6 +16,22 @@ use yii\bootstrap\ActiveForm;
 use yii\helpers\Html;
 use yii\helpers\Url;
 
+$currencies = $this->context->getCurrencyTypes();
+$purses = array_map(function ($k) {
+    return $k->currency;
+}, $model->purses);
+
+$currencies = array_filter(
+    array_combine(array_keys($currencies), array_map(function ($k) {
+        return StringHelper::getCurrencySymbol($k);
+    }, array_keys($currencies))), function ($k) use ($purses) {
+        return in_array($k, $purses, true);
+    }, ARRAY_FILTER_USE_KEY
+);
+
+/// Allowed euro by default
+$currencies['eur'] = '€';
+
 ?>
 
 <?php $form = ActiveForm::begin([
@@ -30,31 +46,16 @@ use yii\helpers\Url;
 
     <?= $form->field($model, 'finance_emails')->textInput(['placeholder' => 'finance@acme.org, ceo@acme.org']) ?>
 
-    <?php if (count($model->purses) > 1) : ?>
-        <?php
-        $currencies = $this->context->getCurrencyTypes();
-        $purses = array_map(function ($k) {
-            return $k->currency;
-        }, $model->purses);
+    <?= $form->field($model, 'autoexchange_to')->dropDownList($currencies)?>
 
-        $currencies = array_filter(
-            array_combine(array_keys($currencies), array_map(function ($k) {
-                return StringHelper::getCurrencySymbol($k);
-            }, array_keys($currencies))), function ($k) use ($purses) {
-                return in_array($k, $purses, true);
-            }, ARRAY_FILTER_USE_KEY);
-        ?>
+    <?= $form->field($model, 'autoexchange_enabled')->checkbox() ?>
 
-        <?= $form->field($model, 'autoexchange_to')->dropDownList($currencies)?>
+    <?= $form->field($model, 'autoexchange_prepayments')->checkbox() ?>
 
-        <?= $form->field($model, 'autoexchange_enabled')->checkbox() ?>
-
-        <?= $form->field($model, 'autoexchange_prepayments')->checkbox() ?>
-
-        <?php if (Yii::$app->user->can('manage')) : ?>
-            <?= $form->field($model, 'autoexchange_force')->checkbox() ?>
-        <?php endif ?>
+    <?php if (Yii::$app->user->can('manage')) : ?>
+        <?= $form->field($model, 'autoexchange_force')->checkbox() ?>
     <?php endif ?>
+
     <hr>
     <?= Html::submitButton(Yii::t('hipanel', 'Save'), ['class' => 'btn btn-success']) ?> &nbsp;
     <?= Html::button(Yii::t('hipanel', 'Cancel'), ['class' => 'btn btn-default', 'data-dismiss' => 'modal']) ?>
