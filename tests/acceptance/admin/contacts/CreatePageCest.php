dbf0e6b64c9a680757b50740a9c9340d62b87c50
strorch <smy980807@ukr.net>
2020-11-18 16:55:07 +0200

    fixed acceptance contact/CreatePageCest

--- tests/acceptance/admin/contacts/CreatePageCest.php
+++ tests/acceptance/admin/contacts/CreatePageCest.php
@@ -19,10 +19,6 @@ class CreatePageCest
 {
     /**
      * @dataProvider testContactData
-     *
-     * @param Admin $I
-     * @param Example $data
-     * @throws \Exception
      */
     public function ensureICanSetEntityDataWithPassport(Admin $I, Example $data): void
     {
@@ -30,21 +26,14 @@ class CreatePageCest
         $I->needPage(Url::to('@contact/create'));
         $I->see('Create contact', 'button');
         $I->executeJS('scroll(0,1000);');
-        $I->click('#passport-data-box button');
         $createPage->fillFormData($data['passport']);
         $I->click('#legal-entity-box button');
-        $I->expectThrowable(InvalidElementStateException::class, function () use ($createPage, $data) {
-            $createPage->fillFormData($data['entity']);
-        });
+        $I->expectThrowable(InvalidElementStateException::class, fn () => $createPage->fillFormData($data['entity']));
         $I->reloadPage();
     }
 
     /**
      * @dataProvider testContactData
-     *
-     * @param Admin $I
-     * @param Example $data
-     * @throws \Exception
      */
     public function ensureICanSetPassportDataWithEntity(Admin $I, Example $data): void
     {
@@ -52,39 +41,38 @@ class CreatePageCest
         $I->needPage(Url::to('@contact/create'));
         $I->see('Create contact', 'button');
         $I->executeJS('scroll(0,1000);');
+        $createPage->fillFormData($data['organization']);
         $I->click('#legal-entity-box button');
         $createPage->fillFormData($data['entity']);
         $I->click('#passport-data-box button');
-        $I->expectThrowable(InvalidElementStateException::class, function () use ($createPage, $data) {
-            $createPage->fillFormData($data['passport']);
-        });
+        $I->expectThrowable(InvalidElementStateException::class, fn () => $createPage->fillFormData($data['passport']));
     }
 
-    /**
-     * @return array
-     */
-    protected function testContactData(): array
+    protected function testContactData(): iterable
     {
-        return [
-            [
-                'passport' => [
-                    'inputs' => [
-                        'birth_date' => '2000-12-12',
-                        'passport_no' => '23213',
-                        'passport_date' => '2001-12-12',
-                        'passport_by' => 'test test',
-                    ],
+        yield [
+            'organization' => [
+                'inputs' => [
+                    'organization'  => 'HiQDev',
                 ],
-                'entity' => [
-                    'inputs' => [
-                        'organization_ru' => '2000-12-12',
-                        'director_name' => '23213',
-                        'inn' => '2001-12-12',
-                        'kpp' => 'test test',
-                    ],
-                    'checkboxes' => [
-                        'isresident' => true,
-                    ],
+            ],
+            'passport' => [
+                'inputs' => [
+                    'birth_date' => '2000-12-12',
+                    'passport_no' => '23213',
+                    'passport_date' => '2001-12-12',
+                    'passport_by' => 'test test',
+                ],
+            ],
+            'entity' => [
+                'inputs' => [
+                    'organization_ru' => '2000-12-12',
+                    'director_name' => '23213',
+                    'inn' => '2001-12-12',
+                    'kpp' => 'test test',
+                ],
+                'checkboxes' => [
+                    'isresident' => true,
                 ],
             ],
         ];
