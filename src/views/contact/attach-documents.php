1f01a70360df4996de726cbedc1537c6e3cd05f5
Yurii Myronchuk <bladeroot@hiqdev.com>
2021-05-19 08:22:20 +0000

    use document widget

--- src/views/contact/attach-documents.php
+++ src/views/contact/attach-documents.php
@@ -15,6 +15,7 @@ use hipanel\widgets\FileInput;
 use yii\helpers\Html;
 use yii\helpers\Inflector;
 use yii\widgets\ActiveForm;
+use hipanel\modules\document\widgets\DocumentFormWidget;
 
 /**
  * @var \hipanel\modules\client\models\Contact
@@ -31,48 +32,34 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="row">
     <div class="col-md-5">
-        <?php Box::begin(['title' => Yii::t('hipanel:client', 'Attach new documents')]); ?>
+        <?php Box::begin([
+            'title' => Yii::t('hipanel:client', 'Attach new documents'),
+        ]) ?>
 
-        <p>
-            <?= Yii::t('hipanel:client',
-                'You can upload copy of your documents in order to help us verify your identity') ?>
-        </p>
+            <p>
+                <?= Yii::t('hipanel:client',
+                    'You can upload copy of your documents in order to help us verify your identity') ?>
+            </p>
 
-        <p>
-            <?= Yii::t('hipanel:client', 'Please make sure that your submitted documents are:') ?>
-        </p>
-        <ul>
-            <li><?= Yii::t('hipanel:client', 'High quality: color image, 150 DPI resolution or higher, not blurry') ?></li>
-            <li><?= Yii::t('hipanel:client', 'Entirely visible: all four corners are captured') ?></li>
-            <li><?= Yii::t('hipanel:client', 'Valid: the expiry date is clearly visible') ?></li>
-        </ul>
+            <p>
+                <?= Yii::t('hipanel:client', 'Please make sure that your submitted documents are:') ?>
+            </p>
+            <ul>
+                <li><?= Yii::t('hipanel:client', 'High quality: color image, 150 DPI resolution or higher, not blurry') ?></li>
+                <li><?= Yii::t('hipanel:client', 'Entirely visible: all four corners are captured') ?></li>
+                <li><?= Yii::t('hipanel:client', 'Valid: the expiry date is clearly visible') ?></li>
+            </ul>
 
-        <?php $form = ActiveForm::begin([
-            'id' => 'attach-form',
-            'enableClientValidation' => true,
-            'validateOnBlur' => true,
-            'options' => ['enctype' => 'multipart/form-data'],
-        ]);
-
-        echo $form->field($model, 'id')->hiddenInput()->label(false);
-        echo $form->field($model, 'title')->textInput([
-            'placeholder' => Yii::t('hipanel:client', 'Passport, ID card, etc'),
-        ]);
-        echo $form->field($model, 'file')->widget(FileInput::class, [
-            'pluginOptions' => [
-                'previewFileType' => 'any',
-                'showRemove' => true,
-                'showUpload' => false,
-            ],
-        ])->label(false); ?>
-
-        <?= Html::submitButton(Yii::t('hipanel', 'Save'), ['class' => 'btn btn-success']); ?>
-
-        <?php $form->end(); ?>
-        <?php Box::end(); ?>
+            <?= DocumentFormWidget::widget([
+                'model' => $model,
+                'boxWidth' => 12,
+            ]) ?>
+        <?php Box::end() ?>
     </div>
     <div class="col-md-7">
-        <?php $box = Box::begin(['renderBody' => false]) ?>
+        <?php $box = Box::begin([
+            'renderBody' => false,
+        ]) ?>
             <?php $box->beginHeader() ?>
                 <?= $box->renderTitle(Yii::t('hipanel:client', 'Documents')) ?>
             <?php $box->endHeader() ?>
@@ -80,7 +67,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 <?= StackedDocumentsView::widget([
                     'models' => $contact->documents,
                     'thumbSize' => 64,
-                ]); ?>
+                ]) ?>
             <?php $box->endBody() ?>
         <?php $box->end() ?>
     </div>
