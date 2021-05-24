f283f821521be9171604cbb20b022e3337afbc90
Drakorgaur <re.markofdark@gmail.com>
2021-05-24 15:51:07 +0300

    Added ClientResourcesCest

---
+++ tests/acceptance/admin/ClientResourcesCest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace hipanel\modules\hosting\tests\acceptance\admin;
+
+use hipanel\helpers\Url;
+use hipanel\tests\_support\Page\IndexPage;
+use hipanel\tests\_support\Page\Widget\Input\Select2;
+use hipanel\tests\_support\Page\Widget\Input\Input;
+use hipanel\tests\_support\Step\Acceptance\Admin;
+
+class DNSzoneCest
+{
+    /**
+     * @var IndexPage
+     */
+    private $index;
+
+    public function _before(Admin $I)
+    {
+        $this->index = new IndexPage($I);
+    }
+
+    public function ensureIndexPageWorks(Admin $I)
+    {
+        $I->login();
+        $I->needPage(Url::to('@client/resource-list'));
+        $I->see('Client resources', 'h1');
+        $this->ensureICanSeeAdvancedSearchBox($I);
+        $this->ensureICanSeeBulkSearchBox();
+    }
+
+    private function ensureICanSeeAdvancedSearchBox(Admin $I)
+    {
+        $this->index->containsFilters([
+            Input::asAdvancedSearch($I, 'Login or Email'),
+            Input::asAdvancedSearch($I, 'Note'),
+            Input::asAdvancedSearch($I, 'Name'),
+            Input::asAdvancedSearch($I, 'Email'),
+            Input::asAdvancedSearch($I, 'Reseller'),
+            Select2::asAdvancedSearch($I, 'Reseller'),
+            Select2::asAdvancedSearch($I, 'Referer'),
+            Select2::asAdvancedSearch($I, 'Types'),
+            Select2::asAdvancedSearch($I, 'States'),
+        ]);
+    }
+
+    private function ensureICanSeeBulkSearchBox()
+    {
+        $this->index->containsColumns([
+            'Object',
+            'Client',
+            'Referral',
+        ]);
+    }
+}
