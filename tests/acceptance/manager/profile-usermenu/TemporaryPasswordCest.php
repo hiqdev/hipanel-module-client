d070c7a8890df25e22ff4c17a4584b59d3951f69
mark <re.markofdark@gmail.com>
2021-11-17 15:51:05 +0100

    New test added - TemporaryPasswordCest

---
+++ tests/acceptance/manager/profile-usermenu/TemporaryPasswordCest.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+namespace hipanel\modules\client\tests\acceptance\manager\profile\usermenu;
+
+use Codeception\Example;
+use hipanel\helpers\Url;
+use hipanel\tests\_support\Page\IndexPage;
+use hipanel\tests\_support\Step\Acceptance\Manager;
+use hipanel\tests\_support\Page\Widget\Input\Input;
+
+class TemporaryPasswordCest
+{
+   public function ensureTemporaryPasswordWorksCorrectly(Manager $I): void
+    {
+        $index = new IndexPage($I);
+        $user = 'hipanel_test_user';
+
+        $I->login();
+        $I->needPage(Url::to('@client'));
+
+        $I->see('Clients', 'h1');
+        Input::asAdvancedSearch($I, 'Login or Email')->setValue($user);
+
+        $I->waitForPageUpdate();
+
+        $index->sortBy('Login');
+        $I->waitForPageUpdate();
+
+        $I->clickLink($user);
+        $I->waitForPageUpdate();
+        $I->see($user, 'h1');
+
+        $this->ensureTemporaryPasswordPopupWorksCorrectly($I);
+    }
+
+    private function ensureTemporaryPasswordPopupWorksCorrectly(Manager $I): void
+    {
+        $I->clickLink('Temporary password');
+        $I->waitForPageUpdate();
+
+        $I->pressButton('Confirm');
+        $I->waitForPageUpdate();
+
+        $I->closeNotification('Temporary password was sent on your email');
+    }
+}
