3b969c9bb54c2b0b478395fbb99d049802e4a0b0
mark <re.markofdark@gmail.com>
2021-11-28 18:06:26 +0100

    Added New Test - BulkAction(Edit)

---
+++ tests/acceptance/manager/ClientBulkEditCest.php
@@ -0,0 +1,107 @@
+<?php
+declare(strict_types=1);
+
+namespace hipanel\modules\client\tests\acceptance\manager;
+
+use hipanel\helpers\Url;
+use hipanel\tests\_support\Page\IndexPage;
+use hipanel\tests\_support\Step\Acceptance\Manager;
+use hipanel\tests\_support\Page\Widget\Input\Dropdown;
+use hipanel\tests\_support\Page\Widget\Input\Input;
+
+class CleintBulkEditCest
+{
+    public function ensureICanDoBulkActionSeveralClients(Manager $I): void
+    {
+        $I->login();
+
+        $I->needPage(Url::to('@client'));
+        $selector = "//table[contains(@class, 'striped')]//tbody";
+
+        $clients = $this->checkIfClientsCanBeDeleted($I, array(1, 2, 3), $selector);
+        $this->ensureICanDoBulkActionOnSelectedUsers($I, $clients, $selector);
+    }
+
+    private function ensureICanDoBulkActionOnSelectedUsers(Manager $I, array $clients, string $baseSelector): void
+    {
+        $this->checkOptions($I, $clients, $baseSelector);
+        $this->ensureICanEditSelectedUsers($I, $clients);
+        $this->ensureUsersWereSuccessfullyEdited($I, $clients, $baseSelector);
+    }
+
+    private function checkOptions(Manager $I, array $clients, $baseSelector): void
+    {
+        foreach ($clients as $client) {
+            $selector = "//tr[$client]//td[1]//input";
+            $I->checkOption($baseSelector . $selector);
+        }
+    }
+
+    private function ensureICanEditSelectedUsers(Manager $I, array $clients): void
+    {
+        $type = 'partner';
+
+        $I->pressButton('Edit');
+        $I->waitForPageUpdate();
+
+        $I->seeInCurrentUrl('client/update');
+        foreach ($clients as $key => $client) {
+            (new Dropdown($I, "#client-$key-type"))->setValue('Partner');
+        }
+
+        $I->pressButton('Save');
+        $I->waitForPageUpdate();
+    }
+
+    private function ensureUsersWereSuccessfullyEdited(Manager $I, array $clients, $baseSelector): void
+    {
+        $indexPage = new IndexPage($I);
+        $I->seeInCurrentUrl('client/client');
+
+        $row = $indexPage->getColumnNumber('Type');
+
+        foreach ($clients as $client) {
+            $I->see('Partner' , $baseSelector . "//tr[$client]//td[$row]");
+        }
+    }
+
+    private function ensureICanBlockSelectedUsers(Manager $I): void
+    {
+        $I->pressButton('Basic actions');
+        $I->clickLink('Enable block');
+        $I->waitForText('Affected clients', 30);
+
+        (new Input($I, 'enable-block-comment'))->setValue('NaN');
+        $I->pressButton('Enable block');
+    }
+
+    private function ensureUsersWereSuccessfullyBlocked(): void
+    {
+
+    }
+
+    private function checkIfClientsCanBeDeleted(Manager $I, array $clients, string $selector): array
+    {
+        $indexPage = new IndexPage($I);
+        $restrictedLogins = array('hipanel_test_user', 'hipanel_test_reseller', 'hipanel_test_manager', 'hipanel_test_admin', 'test_user619924aa32dbcref', 'test_user619923ecf34fdref', 'test_user619923ecf34fd');
+
+        $row = $indexPage->getColumnNumber('Login');
+
+        $availableLogins = true;
+        while ($availableLogins) {
+            foreach ($clients as $key => $client) {
+                $login = $I->grabTextFrom($selector . "//tr[$client]" . "//td[$row]//a[1]");
+                foreach ($restrictedLogins as $restricteLogin) {
+                    if (!strcmp($login, $restricteLogin)) {
+                        $clients[$key] += 3;
+                        $availableLogins = true;
+                        break;
+                    }
+                    $availableLogins = false;
+                }
+                if ($availableLogins) break;
+            }
+        }
+        return $clients;
+    }
+}
