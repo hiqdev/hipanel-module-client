b44bbb39f1957568660d79c7fdffe34747af6dfd
Andrii Klochok <andreyklochok@gmail.com>
2024-02-12 12:17:12 +0200

    HP-1822: refactor `representations`

--- src/Module.php
+++ src/Module.php
@@ -10,26 +10,7 @@
 
 namespace hipanel\modules\client;
 
-use hipanel\grid\RepresentationCollectionFinder;
-use hipanel\grid\RepresentationCollectionFinderInterface;
-use hipanel\grid\RepresentationCollectionFinderProviderInterface;
-use yii\helpers\Inflector;
-use Yii;
-
-class Module extends \hipanel\base\Module implements RepresentationCollectionFinderProviderInterface
+class Module extends \hipanel\base\Module
 {
     public bool $userCreationIsDisabled = false;
-
-    public function getRepresentationCollectionFinder(): RepresentationCollectionFinderInterface
-    {
-        return new class(
-            $this->id, Inflector::id2camel(Yii::$app->controller->id), '\hipanel\modules\%s\grid\%sRepresentations'
-        ) extends RepresentationCollectionFinder {
-            protected function buildClassName()
-            {
-                // Override
-                return parent::buildClassName();
-            }
-        };
-    }
 }
