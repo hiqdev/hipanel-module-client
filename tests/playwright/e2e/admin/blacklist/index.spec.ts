5e1f65ee08efc76fd2bdd5f6e84d2827ac24fa0d
Vadym Hrechukha <vadimgrechuha@gmail.com>
2024-05-30 02:02:40 +0300

    HP-1964 slightly updated Playwright test for blacklist index page

--- tests/playwright/e2e/admin/blacklist/index.spec.ts
+++ tests/playwright/e2e/admin/blacklist/index.spec.ts
@@ -3,6 +3,8 @@ import BlacklistHelper from "@hipanel-module-client/helper/Blacklist";
 import BlacklistCategory from "@hipanel-module-client/helper/category/BlacklistCategory";
 
 test("Correct view Blacklist @hipanel-module-client @admin", async ({ adminPage }) => {
-    const ipHelper = new BlacklistHelper(adminPage);
-    await ipHelper.gotoIndexBlacklist(new BlacklistCategory());
+    const blacklistHelper = new BlacklistHelper(adminPage);
+    await blacklistHelper.gotoIndexBlacklist(new BlacklistCategory());
+
+    await blacklistHelper.hasMainElementsOnIndexPage();
 });
--- tests/playwright/e2e/admin/whitelist/index.spec.ts
+++ tests/playwright/e2e/admin/whitelist/index.spec.ts
@@ -3,6 +3,8 @@ import BlacklistHelper from "@hipanel-module-client/helper/Blacklist";
 import WhitelistCategory from "@hipanel-module-client/helper/category/WhitelistCategory";
 
 test("Correct view Whitelist @hipanel-module-client @admin", async ({ adminPage }) => {
-    const ipHelper = new BlacklistHelper(adminPage);
-    await ipHelper.gotoIndexBlacklist(new WhitelistCategory());
+    const blacklistHelper = new BlacklistHelper(adminPage);
+    await blacklistHelper.gotoIndexBlacklist(new WhitelistCategory());
+
+    await blacklistHelper.hasMainElementsOnIndexPage();
 });
