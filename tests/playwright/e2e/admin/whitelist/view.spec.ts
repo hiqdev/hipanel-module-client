bb93d33853a5cd67ca14425655e8b20cf2275ca1
Vadym Hrechukha <vadimgrechuha@gmail.com>
2024-05-31 10:38:24 +0300

    HP-1964 changed admin credentials to osrc in whitelist view test

--- tests/playwright/e2e/admin/whitelist/view.spec.ts
+++ tests/playwright/e2e/admin/whitelist/view.spec.ts
@@ -2,9 +2,9 @@ import { test } from "@hipanel-core/fixtures";
 import BlacklistHelper from "@hipanel-module-client/helper/Blacklist";
 import WhitelistCategory from "@hipanel-module-client/helper/category/WhitelistCategory";
 
-test("Correct view Whitelist @hipanel-module-client @admin", async ({ adminPage }) => {
+test("Correct view Whitelist @hipanel-module-client @osrc", async ({ osrcPage }) => {
 
-    const blacklistHelper = new BlacklistHelper(adminPage, new WhitelistCategory());
+    const blacklistHelper = new BlacklistHelper(osrcPage, new WhitelistCategory());
 
     await blacklistHelper.gotoIndexBlacklist();
 
