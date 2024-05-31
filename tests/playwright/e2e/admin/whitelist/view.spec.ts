5d4af74956fa1d14f2c02ef223abb5e6a414387c
Vadym Hrechukha <vadimgrechuha@gmail.com>
2024-05-31 09:58:20 +0300

    HP-1964 fixed view whitelist test

--- tests/playwright/e2e/admin/whitelist/view.spec.ts
+++ tests/playwright/e2e/admin/whitelist/view.spec.ts
@@ -2,15 +2,14 @@ import { test } from "@hipanel-core/fixtures";
 import BlacklistHelper from "@hipanel-module-client/helper/Blacklist";
 import WhitelistCategory from "@hipanel-module-client/helper/category/WhitelistCategory";
 
-test("Correct view Whitelist @hipanel-module-client @osrc", async ({ osrcPage }) => {
+test("Correct view Whitelist @hipanel-module-client @admin", async ({ adminPage }) => {
 
-    const blacklistHelper = new BlacklistHelper(osrcPage, new WhitelistCategory());
+    const blacklistHelper = new BlacklistHelper(adminPage, new WhitelistCategory());
 
     await blacklistHelper.gotoIndexBlacklist();
 
-    await blacklistHelper.gotoBlacklistPage(1);
     let blacklist = await blacklistHelper.fillBlacklistFromIndexPage(1);
-
+    await blacklistHelper.gotoBlacklistPage(1);
     await blacklistHelper.checkDetailViewData(blacklist);
 });
 
