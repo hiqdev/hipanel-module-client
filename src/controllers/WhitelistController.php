1e7d74969f2c828dcb749776036bc462db3ee479
Vadym Hrechukha <vadimgrechuha@gmail.com>
2024-05-31 10:35:13 +0300

    HP-1964 removed whitelist views and use blacklist views for Whitelist controller

--- src/controllers/WhitelistController.php
+++ src/controllers/WhitelistController.php
@@ -4,11 +4,17 @@ namespace hipanel\modules\client\controllers;
 
 use hipanel\modules\client\helpers\blacklist\BlacklistCategoryInterface;
 use hipanel\modules\client\helpers\blacklist\WhitelistCategory;
+use Yii;
 
 class WhitelistController extends BlacklistController
 {
     protected function getCategory(): BlacklistCategoryInterface
     {
         return new WhitelistCategory();
+    }
+
+    public function getViewPath()
+    {
+        return Yii::getAlias('@hipanel/modules/client/views/blacklist');
     }
 }
