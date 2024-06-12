4d38f1cb8bbb1a67ba5a0ec67ceadedbaf4d733f
Vadym Hrechukha <vadimgrechuha@gmail.com>
2024-06-12 16:07:01 +0300

    HP-1964: modernized blacklist view playwright test to work without elements on index page

--- tests/playwright/e2e/admin/blacklist/view.spec.ts
+++ tests/playwright/e2e/admin/blacklist/view.spec.ts
@@ -1,15 +1,30 @@
 import { test } from "@hipanel-core/fixtures";
 import BlacklistHelper from "@hipanel-module-client/helper/Blacklist";
 import BlacklistCategory from "@hipanel-module-client/helper/category/BlacklistCategory";
+import Blacklist from "@hipanel-module-client/model/Blacklist";
+
+const blacklistItemForTestView = new Blacklist();
+blacklistItemForTestView.name = 'blacklist_test_view_item';
+blacklistItemForTestView.type = 'Domain';
+blacklistItemForTestView.message = 'Test Blacklist view page';
+blacklistItemForTestView.showMessage = 'Yes';
 
 test("Correct view Blacklist @hipanel-module-client @admin", async ({ adminPage }) => {
-
     const blacklistHelper = new BlacklistHelper(adminPage, new BlacklistCategory());
 
     await blacklistHelper.gotoIndexBlacklist();
+
+    if (await blacklistHelper.getRowsOnIndexPage() === 0) {
+        blacklistItemForTestView.id = await blacklistHelper.createBlacklist(new BlacklistCategory(), blacklistItemForTestView);
+        await blacklistHelper.gotoIndexBlacklist();
+    }
 
     const blacklist = await blacklistHelper.fillBlacklistFromIndexPage(1);
     await blacklistHelper.gotoBlacklistPage(1);
 
     await blacklistHelper.checkDetailViewData(blacklist);
+
+    if (blacklistItemForTestView.id) {
+        await blacklistHelper.deleteBlacklist(blacklistItemForTestView.id);
+    }
 });
