33d7b06ea1942893e8ad2c7feedce46d2ebba6dd
Andrii Klochok <andreyklochok@gmail.com>
2025-11-11 10:10:10 +0200

    Fix typo in client export test description

--- tests/playwright/e2e/manager/contact/export.spec.ts
+++ tests/playwright/e2e/manager/contact/export.spec.ts
@@ -1,7 +1,7 @@
 import { test } from "@hipanel-core/fixtures";
 import Index from "@hipanel-core/page/Index";
 
-test("Client export is works correctly @hipanel-module-client @manager", async ({ page }) => {
+test("Client export works correctly @hipanel-module-client @manager", async ({ page }) => {
   await page.goto("/client/contact/index");
 
   const indexPage = new Index(page);
